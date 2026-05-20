7bbbcdc6f0d1920cac8e5d1578eef4fcd8336ce1
SyaefulBachtiar <syaefulbachtiar2@gmail.com>
2026-05-20 08:55:18 +0700

    update

--- resources/views/livewire/profile/update-profile-information-form.blade.php
+++ resources/views/livewire/profile/update-profile-information-form.blade.php
@@ -33,7 +33,7 @@ new class extends Component
     {
         $user = Auth::user();
         $this->email = Auth::user()->email;
-        $this->current_avatar = Auth::user()->profile_photo_url;
+        $this->current_avatar = $user->avatar;
 
         $validated = $this->validate([
             'name' => ['required', 'string', 'max:255'],
